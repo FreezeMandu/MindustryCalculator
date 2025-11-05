a3f3570dcea5b481bb0cb7183b4951bb936aa498
FreezeMandu <sewolminsuk03@gmail.com>
2025-11-05 17:54:29 +0900

    잘못된 입력 예외처리

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "drill.h" //드릴과 펌프의 출력량 반환
 #include "factory.h" //요구량 계산 & 출력
 
@@ -6,19 +7,29 @@ int Fac();
 
 int main() {
 	printf("=== Mindustry 공장 비율 계산기 ===\n");
-	printf("1. 별 다른 표기가 없으면 필요량 입출력 단위는 '/초' 입니다\n2. 정수만 입력하세요. 무한루프 들어가면 전 모릅니다.\n\n"); //2. 야매 if문 넣고 에러 무시해야 하나?
-	int one;
+	printf("1. 별 다른 표기가 없으면 필요량 입출력 단위는 '/초' 입니다\n\n");
+	int menu;
+	char one[10], scan[10] = { 0 };
 
 	while (1) {
+		int cnt = 0; //scan[] 카운터 초기화
 		printf("[메인] 범주 선택 & 종료\n");
 		printf("1. 공장\n2. 발전기(예정)\n3. 프로그램 종료\n>> ");
-		scanf_s("%d", &one);
+		scanf_s(" %s", &one, 10);
+		for (int i = 0; i < 10; i++) {
+			scan[cnt] = 0; //정수 변환용 배열 초기화
+			if (one[i] != '\0' && 48 <= one[i] && one[i] <= 57) {
+				scan[cnt] = one[i];
+			}
+			cnt++;
+		}
+		menu = atoi(scan);
 		printf("\n\n");
 
-		if (one == 3)
+		if (menu == 3)
 			break;
 		
-		if (one == 1) {
+		if (menu == 1) {
 			Fac();
 		}
 	}
@@ -26,23 +37,33 @@ int main() {
 }
 
 int Fac() {
-	int two, ea;
-	char end;
+	int menu, ea;
+	char two[10], scan[10] = { 0 }, end;
 	while (1) {
+		int cnt = 0; //scan[] 카운터 초기화
 		printf("[공장] 선택\n");
 		printf("1. 흑연 압축기\n2. 대형 흑연 압축기\n3. 실리콘 제련소\n4. 실리콘 도가니\n5. 가마\n6. 플라스터늄 압축기\n7. 메타 제조기\n8. 설금 제련소\n9. 냉각수 혼합기\n");
 		printf("10. 파이라타이트 혼합기\n11. 폭발물 혼합기\n12. 융해기\n13. 광재 분리기\n14. 광재 분해기\n15. 포자 압축기\n16. 분쇄기\n17. 석탄 정제기");
 		printf("\n99. 메인으로 나가기\n>> ");
-		scanf_s("%d", &two);
+		scanf_s(" %s", &two, 10);
+		for (int i = 0; i < 10; i++) {
+			scan[cnt] = 0; //정수 변환용 배열 초기화
+			if (two[i] != '\0' && 48 <= two[i] && two[i] <= 57) { //아스키코드 0 ~ 9에 해당하면 ~
+				scan[cnt] = two[i];
+			}
+			cnt++;
+		}
+		menu = atoi(scan);
+		printf("[menu] %d\n", menu);
 		printf("\n");
 
-		if (two == 99)
+		if (menu == 99)
 			return 0;
 		printf("공장 갯수를 입력하시오 : ");
 		scanf_s("%d", &ea);
 		printf("\n");
 
-		switch (two) {
+		switch (menu) {
 		case 1: g_press(ea); break;
 		case 2: m_press(ea); break;
 		case 3: silicon(ea); break;
--- test.c
+++ test.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+	int cnt = 0, menu;
 	char one[10], scan[10] = { 0 };
-	scanf_s("%s", &one, 10);
+	scanf_s(" %s", &one, 10);
 
 	for (int i = 0; i < 10; i++) {
-		if (one[i] != '\0') {
-			//scan[i] = one[i];
+		if (one[i] != '\0' && 48 <= one[i] && one[i] <= 57) {
+			scan[cnt++] = one[i];
 		}
 	}
-	printf("%s", scan);
+	menu = atoi(scan);
+
+	switch (menu) {
+	case 1:
+		printf("input 1\n");
+		break;
+	case 23:
+		printf("input 23\n");
+		break;
+	}
 
 	return 0;
 }
 
 //아스키 48 ~ 57 -> 0 ~ 9
+//1. 배열 1을 아스키와 비교하여 숫자가 맞는가? 맞으면 배열 2에 넣기(0은 예외로 함). 배열 2 카운트는 별개로 진행함
+//2. 배열 2를 정수로 바꿔 배열이 아닌 변수에 저장
+//테스트 완료.
